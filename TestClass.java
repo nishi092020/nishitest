908eb048dd0b537c5913a5ff5a7d94d25be862f5
nishi092020 <nishigandhagaikwad12@gmail.com.com>
2020-02-17 09:19:34 +0530

    TestGITHUB

---
+++ TestClass.java
@@ -0,0 +1,13 @@
+package Com.Sel.Xcel1;
+
+import java.util.Scanner;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s1=new Scanner(System.in);
+		int i1=s1.nextInt();
+	}
+
+}
